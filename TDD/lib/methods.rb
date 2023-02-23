24dfe920b1d8963bfb6e693555850146d3bcb88f
Taisia Karaseva <taisiat@gmail.com>
2023-02-23 14:58:42 -0800

    Add initial logic for two_sum

--- TDD/lib/methods.rb
+++ TDD/lib/methods.rb
@@ -7,3 +7,15 @@ def uniq(arr)
 
     answer
 end
+
+class Array
+     def two_sum
+        answer = []
+        self.each.with_index do |ele1, ix1|
+            self.each.with_index do |ele2, ix2|
+                answer << [ix2, ix1] if (ele1 + ele2) == 0 #&& ix2 > ix1
+            end
+        end
+        answer
+     end
+end
--- TDD/spec/methods_spec.rb
+++ TDD/spec/methods_spec.rb
@@ -25,3 +25,24 @@ describe "#uniq" do
         expect(uniq([1, 2, 1, 3, 3])).to eq([1,2,3])
     end
 end
+
+describe "Array#two_sum" do
+    subject(:arr) { [-1, 0, 2, -2, 1] }
+
+    it "returns a 2D array" do
+        expect(arr.two_sum).to be_a(Array)
+        expect(arr.two_sum[0]).to be_a(Array)
+    end
+
+    it "returns array of coordinates where eles at those coords add up to zero" do
+        answer = arr.two_sum
+        expect(answer.all? { |coords| (arr[coords[0]] + arr[coords[1]]) == 0}).to be(true)
+    end
+
+    it "returns pairs where the first coordinate is less than the second"
+
+    it "returns coords where the first coordinate of a set is less than the first coord of the next second"
+
+    it "only returns unique pairs of coords"
+
+end
