f595cda75b37d706f7e79d5cdb29c0813e89a775
bird <mbird3@gmail.com>
2023-02-23 17:34:33 -0800

    write Towers initialize method and tests as well as beginning of won logic

--- TDD/lib/towers.rb
+++ TDD/lib/towers.rb
@@ -1,7 +1,30 @@
 class Towers
 
     attr_reader :stacks
+    
     def initialize(height)
         @stacks = Array.new(3) {Array.new}
+
+        height.downto(1) do |i|
+            @stacks[0] << i
+        end
+    end
+
+    def won?
+        true
+    end
+
+    def move(from_stack, to_stack)
+        if valid?(from_stack) && valid?(to_stack)
+        end
+        #add logic to ensure from stack has elements
+        #add logic for popping and pushing
+    end
+
+
+
+    private
+    def valid?(stack_idx)
+        stack_idx.between?(0,2)
     end
 end
--- TDD/spec/towers_spec.rb
+++ TDD/spec/towers_spec.rb
@@ -11,15 +11,31 @@ describe Towers do
             expect(tower.stacks.length).to eq(3)
         end
 
-        # it "starts the first stack in ascending order" do
-        #     expect()
-        # end
+        it "starts the first stack in ascending order" do
+            expect(tower.stacks[0]).to eq([5,4,3,2,1])
+        end
         
-        it "sets 2 stacks as empty"
+        it "sets 2 stacks as empty" do
+            expect(tower.stacks[1]).to be_empty
+            expect(tower.stacks[2]).to be_empty
+        end
+
         it "creates height number of elements" do
             expect(tower.stacks[0].length).to eq(5)
         end
 
     end
+
+    describe "Towers#won?" do
+    
+        it "return false if all elements are not in the last stack" do
+            expect(tower.won?).to be(false)
+        end
+
+        it "return false if all elements are not in order"
+
+        it "returns true if all elements are in order in the last stack"
+    end
+
 end
 
